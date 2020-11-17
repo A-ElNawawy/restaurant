bd3fbd34d67c0b4fa43d51e6f655062d6e890743
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-17 22:23:43 +0200

    create outline components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,26 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      my App
+      <header>
+        <nav className="OnFlyContacts">OnFlyContacts</nav>
+        <nav className="MainNav">nav</nav>
+      </header>
+      <section className="Body">
+        Body
+      </section>
+      <footer>
+        <div className="Newsletter">newsletter</div>
+        <div className="Summery">
+          <div>About us</div>
+          <div>Recent Posts</div>
+          <div>Our Working Hours</div>
+        </div>
+        <div className="CopyRight">
+          <div>Social</div>
+          <div>logo</div>
+          <div>copy right</div>
+        </div>
+      </footer>
     </div>
   );
 };
