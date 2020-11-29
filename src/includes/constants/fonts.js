20fd513719c25f524052e6844ef8034c290bbf78
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-29 21:03:40 +0200

    adding muli font

--- src/includes/constants/fonts.js
+++ src/includes/constants/fonts.js
@@ -1,1 +1,3 @@
-export const body_font = '60px';
+export const body_font_family = "'Lora', serif";
+export const logo_font_family = "'Great Vibes', cursive";
+export const p_font_family = "'Muli', sans-serif";
