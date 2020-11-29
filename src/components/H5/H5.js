08006150608fd5e7de82a45ed07672108eb36c0a
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-29 21:01:29 +0200

    Create H5 Component

---
+++ src/components/H5/H5.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+const H5 = styled.h5`
+  font-size: 18px;
+  /*font-weight: 700;*/
+  line-height: 1.5;
+  ::first-letter{
+    text-transform: capitalize;
+  }
+`;
+export default H5;
