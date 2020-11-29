af61fda13afd793d1802f00085b59084f8f0f550
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-11-29 21:03:03 +0200

    adding demo data

--- src/includes/langs/english.js
+++ src/includes/langs/english.js
@@ -1,1 +1,2 @@
 export const paragraph = 'Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationesdemonstraverunt lectores legere me lius quod ii legunt saepius.';
+export const paragraph2 = 'Aliquam ullamcorper finibus lorem ut auctor. Aliquam sed aliquet eros. Vivamus maximus dignissim mi et accumsan.';
