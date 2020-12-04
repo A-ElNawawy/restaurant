08ec00476b09c332dd09b7e953141294661d5746
A-ElNawawy <eng.ahmed.elnawawy@gmail.com>
2020-12-04 23:41:43 +0200

    managing Images

---
+++ src/demoData/images/index.js
@@ -0,0 +1,26 @@
+import bg_counters from './bg_counters.jpg';
+import bg_street from './bg_street.jpg';
+import bg_vegetables from './bg_vegetables.jpg';
+import bg_wood from './bg_wood.jpg';
+import blog1 from './blog1.jpg';
+import blog3 from './blog3.jpg';
+import blog2 from './blog2.jpg';
+import aboutus from './aboutus.png';
+import features1 from './features1.jpg';
+import features2 from './features2.png';
+import features3 from './features3.png';
+import small_wood from './small_wood.jpg';
+export {
+  bg_counters,
+  bg_street,
+  bg_vegetables,
+  bg_wood,
+  blog1,
+  blog2,
+  blog3,
+  aboutus,
+  features1,
+  features2,
+  features3,
+  small_wood
+};
